302a3e3086147519eea00b66d03ec7cf81619db6
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-11-27 16:33:25 -0500

    Initial Commit

---
+++ oneDayAtATime/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  oneDayAtATime
+//
+//  Created by Marty Hernandez Avedon on 11/27/17.
+//  Copyright © 2017 Marty Hernandez Avedon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
