55c47d103f267668a8ccf7df2c246a6a80281163
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-01-10 13:42:36 -0500

    /attempting/ a sensible way of loading and retrieving data

--- oneDayAtATime/Supporting Files/StorageManager.swift
+++ oneDayAtATime/Supporting Files/StorageManager.swift
@@ -12,20 +12,24 @@ enum StorageError: Error {
     case pathNotFound
 }
 
-class DataStore {
-    private init(){}
+enum PathName: String {
+    case Week1, Week2, Week3
+}
+
+final class DataStore {
     static let manager = DataStore()
-    static let pathName = "Days.plist"
+    var pathName: String = "Week1.plist"
+    
+    private init(){}
     
     private var lists = [String: Checklist]() {
         didSet {
-            do {
-                try self.storeToDisk()
-            }
-            catch {
-                print(error.localizedDescription)
-            }
+            self.storeToDisk()
         }
+    }
+    
+    func select(filepath path: PathName) {
+        self.pathName = "\(path.rawValue).plist"
     }
 
     func findDocumentsDirectory() -> URL {
@@ -40,32 +44,62 @@ class DataStore {
         return DataStore.manager.findDocumentsDirectory().appendingPathComponent(path)
     }
     
-    func storeToDisk() throws {
+    func storeToDisk() {
         let encoder = PropertyListEncoder()
         
         do {
             let data = try encoder.encode(lists)
-            try data.write(to: self.find(filepath: DataStore.pathName), options: .atomic)
+            try data.write(to: self.find(filepath: pathName), options: .atomic)
         }
         catch {
-            throw StorageError.pathNotFound
+            print(StorageError.pathNotFound.localizedDescription)
         }
     }
     
-    private func save(list: Checklist, to day: String) {
+    func save(list: Checklist, to day: String) {
         self.lists[day] = list
+    }
+    
+    func retrieve(list day: String) -> Checklist? {
+        return self.lists[day]
+    }
+    
+    func retrieveWeek() -> [String: Checklist] {
+        return lists
+    }
+    
+    func retrieveWeek(_ num: Int) -> [String: Checklist] {
+        let currentPathName = self.pathName.components(separatedBy: ".").first!
+        var tempPathName: PathName
+        
+        switch num {
+        case 3:
+            tempPathName = .Week3
+        case 2:
+            tempPathName = .Week2
+        default:
+            tempPathName = .Week1
+        }
+        
+        defer {
+            select(filepath: PathName(rawValue: currentPathName)!)
+        }
+        
+        select(filepath: tempPathName)
+        return retrieveWeek()
     }
     
     func add(item: String, to day: String) {
         let newItem = ListItem(title: item, checkedOff: false)
-        var newList = self.lists[day] ?? Checklist()
+        var newList = retrieve(list: day) ?? Checklist()
+        
         newList.append(newItem)
         
         self.save(list: newList, to: day)
     }
     
     func delete(itemAtIndex index: Int, from day: String) {
-        guard let list = lists[day] else {
+        guard let list = retrieve(list: day) else {
             return
         }
         
@@ -81,7 +115,7 @@ class DataStore {
     }
     
     func update(itemAtIndex index: Int, from day: String, withNewItem newItem: ListItem) {
-        guard let list = lists[day] else {
+        guard let list = retrieve(list: day) else {
             return
         }
         
@@ -98,7 +132,7 @@ class DataStore {
     
     func edit(itemAtIndex index: Int, from day: String, withNewValue newValue:
         String) {
-        guard let list = lists[day] else {
+        guard let list = retrieve(list: day) else {
             return
         }
         
@@ -117,7 +151,7 @@ class DataStore {
     
     func edit(itemAtIndex index: Int, from day: String, withNewValue newValue:
         Bool) {
-        guard let list = lists[day] else {
+        guard let list = retrieve(list: day) else {
             return
         }
         
@@ -132,5 +166,18 @@ class DataStore {
         newList[index] = newItem
         
         self.save(list: newList, to: day)
+    }
+    
+    func load() {
+        let path = find(filepath: pathName)
+        let decoder = PropertyListDecoder()
+        
+        do {
+            let data = try Data.init(contentsOf: path)
+            self.lists = try decoder.decode([String: Checklist].self, from: data)
+        }
+        catch {
+            print(error.localizedDescription)
+        }
     }
 }
